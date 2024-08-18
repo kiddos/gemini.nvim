d0c9b004f6c96496e885c65a42f7cfeae077be0e
kiddos <kiddo831007@gmail.com>
2024-08-19 00:44:57 +0800

    check if curl exists

--- lua/gemini.lua
+++ lua/gemini.lua
@@ -3,6 +3,11 @@ local config = require('gemini.config')
 local M = {}
 
 M.setup = function(opts)
+  if not vim.fn.executable('curl') then
+    vim.notify('curl is not found', vim.log.levels.WARN)
+    return
+  end
+
   config.set_config(opts)
 
   require('gemini.chat').setup()
