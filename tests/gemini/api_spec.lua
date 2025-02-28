587fcc10e3fad582561ec5fc41086b7974fbca2e
kiddos <kiddo831007@gmail.com>
2025-02-28 08:10:45 +0800

    add tests for long messages

--- tests/gemini/api_spec.lua
+++ tests/gemini/api_spec.lua
@@ -20,4 +20,24 @@ describe('api', function()
       'parts', 1, 'text' })
     assert(#model_response > 0)
   end)
+
+  it('should send long message', function()
+    local generation_config = {
+      temperature = 0.9,
+      top_k = 1.0,
+      max_output_tokens = 2048,
+      response_mime_type = 'text/plain',
+    }
+    local long_message = string.rep('this is a very very long message ', 3000)
+    local future = api.gemini_generate_content(long_message, nil, api.MODELS.GEMINI_2_0_FLASH, generation_config, nil)
+    local result = future:wait()
+    local stdout = result.stdout
+    print(stdout)
+    assert(#stdout > 0)
+
+    local result = vim.json.decode(stdout)
+    local model_response = util.table_get(result, { 'candidates', 1, 'content',
+      'parts', 1, 'text' })
+    assert(#model_response > 0)
+  end)
 end)
