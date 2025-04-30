922abb9c69c2cd66547df6b07a178091dd63e44a
kiddos <kiddo831007@gmail.com>
2025-04-30 22:35:55 +0800

    check neovim version

--- lua/gemini.lua
+++ lua/gemini.lua
@@ -2,9 +2,28 @@ local config = require('gemini.config')
 
 local M = {}
 
+local function is_nvim_version_ge(major, minor, patch)
+  local v = vim.version()
+  if v.major > major then
+    return true
+  elseif v.major == major then
+    if v.minor > 9 then
+      return true
+    elseif v.minor == minor and v.patch >= patch then
+      return true
+    end
+  end
+  return false
+end
+
 M.setup = function(opts)
   if not vim.fn.executable('curl') then
     vim.notify('curl is not found', vim.log.levels.WARN)
+    return
+  end
+
+  if not is_nvim_version_ge(0, 9, 1) then
+    vim.notify('neovim version too old', vim.log.levels.WARN)
     return
   end
 
