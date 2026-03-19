a7d5e9e340e840b727db5915cb6e9f20e337f7b9
kiddos <kiddo831007@gmail.com>
2026-03-20 00:41:05 +0800

    add models

--- lua/gemini/api.lua
+++ lua/gemini/api.lua
@@ -5,7 +5,11 @@ local M = {}
 local API = "https://generativelanguage.googleapis.com/v1beta/models/";
 
 M.MODELS = {
+  GEMINI_3_FLASH_PREVIEW = 'gemini-3-flash-preview',
   GEMINI_FLASH_LATEST = 'gemini-flash-latest',
+  GEMINI_3_1_PRO_PREVIEW = 'gemini-3.1-pro-preview',
+  GEMINI_3_1_FLASH_LITE_PREVIEW = 'gemini-3.1-flash-lite-preview',
+  GEMINI_FLASH_LITE_LATEST = 'gemini-flash-lite-latest',
   GEMINI_2_5_PRO = 'gemini-2.5-pro',
   GEMINI_2_5_FLASH = 'gemini-2.5-flash',
   GEMINI_2_5_FLASH_LITE = 'gemini-2.5-flash-lite',
